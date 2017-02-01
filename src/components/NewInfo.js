3d1fddd53e3dceaa0e0c7153c656984a0894d8fa
HONG ZHOU <holly_zhou@yahoo.com>
2017-01-31 19:53:59 -0700

    fixed it

--- src/components/NewInfo.js
+++ src/components/NewInfo.js
@@ -63,12 +63,12 @@ class NewInfo extends Component {
 
   render() {
     return (
-      <form onSubmit={this._onSubmit}>
+      <form onSubmit={this._onSubmit} >
         <div className="row">
           <div className="form-group">
-            <label className="col-sm-2 control-label">First Name:</label>
+            <label className="col-sm-2 control-labe">First Name:</label>
             <div className="col-sm-4">
-              <input type="text" className="form-control" placeholder="Holly" ref="firstName"/>
+              <input type="text" className="form-control" placeholder="Holly" ref="firstName" required/>
             </div>
           </div>
         </div>
@@ -77,18 +77,20 @@ class NewInfo extends Component {
           <div className="form-group">
             <label className="col-sm-2 control-label">Last Name:</label>
             <div className="col-sm-4">
-              <input type="text" className="form-control" placeholder="Zhou" ref="lastName"/>
+              <input type="text" className="form-control" placeholder="Zhou" ref="lastName" required/>
             </div>
           </div>
         </div>
         <br/>
         <h4>Hobbies:</h4>
-          <Checkbox checkedIcon={<ActionFavorite />} uncheckedIcon={<ActionFavoriteBorder />} label="Sports" value="Sports" style={styles.checkbox} onCheck={this.getHobbies} />
-          <Checkbox checkedIcon={<ActionFavorite />} uncheckedIcon={<ActionFavoriteBorder />} label="Music" value="Music" style={styles.checkbox} onCheck={this.getHobbies} />
-          <Checkbox checkedIcon={<ActionFavorite />} uncheckedIcon={<ActionFavoriteBorder />} label="Traveling" value="Traveling" style={styles.checkbox} onCheck={this.getHobbies} />
-          <Checkbox checkedIcon={<ActionFavorite />} uncheckedIcon={<ActionFavoriteBorder />} label="Painting" value="Painting" style={styles.checkbox} onCheck={this.getHobbies} />
-          <Checkbox checkedIcon={<ActionFavorite />} uncheckedIcon={<ActionFavoriteBorder />} label="Computer" value="Computer" style={styles.checkbox} onCheck={this.getHobbies} />
-          <Checkbox checkedIcon={<ActionFavorite />} uncheckedIcon={<ActionFavoriteBorder />} label="Reading" value="Reading" style={styles.checkbox} onCheck={this.getHobbies} />
+          <div className="checkboxes">
+            <Checkbox checkedIcon={<ActionFavorite />} uncheckedIcon={<ActionFavoriteBorder />} label="Sports" value="Sports" style={styles.checkbox} onCheck={this.getHobbies} />
+            <Checkbox checkedIcon={<ActionFavorite />} uncheckedIcon={<ActionFavoriteBorder />} label="Music" value="Music" style={styles.checkbox} onCheck={this.getHobbies} />
+            <Checkbox checkedIcon={<ActionFavorite />} uncheckedIcon={<ActionFavoriteBorder />} label="Traveling" value="Traveling" style={styles.checkbox} onCheck={this.getHobbies} />
+            <Checkbox checkedIcon={<ActionFavorite />} uncheckedIcon={<ActionFavoriteBorder />} label="Painting" value="Painting" style={styles.checkbox} onCheck={this.getHobbies} />
+            <Checkbox checkedIcon={<ActionFavorite />} uncheckedIcon={<ActionFavoriteBorder />} label="Computer" value="Computer" style={styles.checkbox} onCheck={this.getHobbies} />
+            <Checkbox checkedIcon={<ActionFavorite />} uncheckedIcon={<ActionFavoriteBorder />} label="Reading" value="Reading" style={styles.checkbox} onCheck={this.getHobbies} />
+          </div>
         <button type="submit" className="btn btn-success">Sumbit</button>
       </form>
 
